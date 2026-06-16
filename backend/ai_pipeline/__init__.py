e8567e5d3aeba9ff72a408072811f2afed0f02f9
K Sandeep Kumar Reddy <ksandeepkumarreddy963@gmail.com>
2026-06-16 21:46:45 +0530

    Delete backend directory

--- backend/ai_pipeline/__init__.py
+++
@@ -1,5 +0,0 @@
-# Empty file
-# This file tells Python that the 'ai_pipeline' folder is a Python package
-# This allows views.py to import from it like:
-# from ai_pipeline.parser import parse_resume
-# from ai_pipeline.learning_path import generate_learning_path
--- backend/core/__init__.py
+++
@@ -1,3 +0,0 @@
-# Empty file
-# This file tells Python that the 'core' folder is a Python package
-# Django needs this to recognize and import from this folder
